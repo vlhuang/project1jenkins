f4370bcc876d1f9a075ff4bf78ec41da0cc3824d
vlhuang <vlhuang88@yahoo.com>
2021-02-18 05:13:12 +0000

    Adding logic and test

---
+++ bookzy/src/main/java/com/simplilearn/bookzy/BookzyApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.bookzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzyApplication.class, args);
+	}
+
+}
---
+++ bookzy/src/test/java/com/simplilearn/bookzy/BookzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.bookzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
